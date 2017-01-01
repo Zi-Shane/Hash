f62404891fadc5d9d00a1a46b2463049bf11b45f
Zi-Shane.MacBookPro <shane110319@gmail.com>
2017-01-02 00:50:13 +0800

    finish hash

--- hash/main.cpp
+++ hash/main.cpp
@@ -11,6 +11,7 @@
 
 using namespace std;
 
+// determine table size
 int table_size(int items_num) {
     int size;    // table size
     items_num *= 2;
@@ -28,6 +29,37 @@ int table_size(int items_num) {
         }
     }
     return size = --i;
+}
+// probing func
+int probing_q(int i) {
+    int probing = i * i;
+    return probing;
+}
+// check collision
+bool isCollision(int *table, int index){
+    bool collision = true;
+    return collision = (table[index] == 0 ? false : true);
+}
+// determine hashed value
+int hash_function(int origin, int size, int *table, int probing_times = 0) {
+    int hashed;
+    
+    while (probing_times < 100) {
+        hashed = (origin + probing_q(probing_times)) % size;
+        
+        if (isCollision(table, hashed))
+            probing_times++;
+        else
+           // *probing_total += probing_times;
+            return hashed;
+    }
+    return -1;    //probing failed
+}
+// print hashed table
+void print_table(int *table, int size) {
+    for (int i = 0; i < size; i++) {
+        cout << "Table[" << i << "]\t" << table[i] << endl;
+    }
 }
 
 int main(void) {
@@ -51,20 +83,53 @@ int main(void) {
     }
     file.close();
 
-
-    int hash_Table[table_size(count_items)];
-    
-    // push into hash table
-    for (int i = 0; i < count_items; i++) {
-        hash_Table[i] = file_items[i];
+    int size = table_size(count_items);
+    int hash_Table[size];    // create a hash table
+    for (int i = 0; i < size; i++) {
+        hash_Table[i] = 0;
     }
+    
+//    int *probing_times_total = 0;
+    
+    for (int i = 0; i < count_items; i++) {
+        int hashed_index = hash_function(file_items[i], size, hash_Table);
+        if (hashed_index == -1)
+            cout << "probing failed!" << hashed_index << endl;
+        else
+            hash_Table[hashed_index] = file_items[i];
+    }
+    
+    print_table(hash_Table, size);
+    
+    
+    
+  //  cout << "\nTotal probing times = " << probing_times_total << endl;
+    
 
 //      Debug
 
-//    //check table size
+//    // check how many items in hashed table & table size
+//    cout << "\n";
+//    int j = 0;
+//    for (int i = 0; i < size; i++) {
+//        cout << hash_Table[i] << ",";
+//        if (hash_Table[i] != 0) {
+//            j++;
+//        }
+//    }
+//    cout << "\n" << j << "," << size;
+    
+//    // push into hash table
+//    int hash_Table[table_size(count_items)];
+//    for (int i = 0; i < count_items; i++) {
+//        hash_Table[i] = file_items[i];
+//    }
+    
+    
+//    // check table size
 //    cout << sizeof(hash_Table) / sizeof(*hash_Table);
     
-//    //print
+//    // print read file
 //    for (int i = 0; i < count_items; i++) {
 //        cout << hash_Table[i] << endl;
 //    }
