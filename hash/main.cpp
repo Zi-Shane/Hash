6319731d6f04f09b4196b61cde360088e42ed241
Zi-Shane.MacBookPro <shane110319@gmail.com>
2017-01-01 15:33:40 +0800

    read file in array

--- hash/main.cpp
+++ hash/main.cpp
@@ -7,9 +7,33 @@
 //
 
 #include <iostream>
+#include <fstream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+int main(void) {
+    string file_name;
+    int hash_Table[100];
+    int count = 0;
+    int index = 0;
+    
+    cout << "file name: ";
+    cin >> file_name;
+    
+    //read and open file
+    ifstream file ( file_name , ifstream::in );
+    //file open failed
+    if (!file) {
+        cout << "An error occured when opening file!";
+        return EXIT_FAILURE;
+    }
+    //file open successed
+    while (file >> hash_Table[index]) {
+        count++;
+        index++;
+    }
+    file.close();
+
+    
+    return EXIT_SUCCESS;
 }
